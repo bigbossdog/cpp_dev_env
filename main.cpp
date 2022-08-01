78716d7cdba93c7d79dcf93c422371d909f6231e
GeraldoOfRivera <23650385+GeraldoOfRivera@users.noreply.github.com>
2022-08-01 11:05:55 -0400

    initial commit - clangd language server up (disable default c/cpp intellisense) and debugging works as expected

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// basic playground space
+
+using namespace std;
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n" << std::endl;
+    int y = 2;
+    int x = 2;
+    std::cout << "Hello, again!\n" << endl;
+    return 0;
+}
